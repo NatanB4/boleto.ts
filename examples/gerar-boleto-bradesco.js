c88d433118f86bace8d420e213005e5b96a1550f
natanb4 <natanbarbosa027@gmail.com>
2024-04-18 15:20:50 -0300

    feat: re-base code

--- examples/gerar-boleto-bradesco.js
+++ examples/gerar-boleto-bradesco.js
@@ -1,70 +1,62 @@
-const { randomUUID } = require('crypto');
-const {Bancos, Boletos, streamToPromise} = require('../lib/index');
-const fs = require('fs');
+const {Bancos, Boletos } = require('../lib/index');
 
 const boleto = {
-        banco: new Bancos.Bradesco(),
-        pagador: {
-          nome: 'NATO E NANDA MODA INFANTO JUVENIL LTDA EPP',
-          registroNacional: '04926156000159',
-          endereco: {
-            logradouro: 'R CATETE, 821',
-            bairro: 'CATETE',
-            cidade: 'EUGENOPOLIS',
-            estadoUF: 'MG',
-            cep: '36855000'
-          }
-        },
-        instrucoes: [
-          'JUROS POR DIA DE ATRASO R$ 0,88',
-          'APOS VENCIMENTO, MULTA DE R$ 1,5',
-          '',
-          'Caso não seja efetuado o pagamento em até 5 dias úteis após o vencimento,',
-          'o mesmo será protestado em cartório.'
-        ],
-        beneficiario: {
-          nome: 'RIOS COMERCIO IMPORTACAO E EXPORTACAO LTDA',
-          cnpj: '38875449000176',
-          dadosBancarios: {
-            carteira: '09',
-            agencia: '549',
-            agenciaDigito: '9',
-            conta: '0098500',
-            contaDigito: '0',
-            nossoNumero: '00000058912',
-            nossoNumeroDigito: '0'
+        banco: new Bancos.Itau(),
+          pagador: {
+            nome: 'EFM COMERCIO DE MATERIAIS DE C',
+            RegistroNacional: '30.340.910/0001-84',
+            endereco: {
+              logradouro: 'R CONSELHEIRO FRANCO, 295',
+              bairro: 'CENTRO',
+              cidade: 'FEIRA DE SANTAN',
+              estadoUF: 'BA',
+              cep: '44002128'
+            }
           },
-          endereco: { logradouro: '', bairro: '', cidade: '', estadoUF: '', cep: '' }
-        },
-        boleto: {
-          locaisDePagamentos: ['Pagável em qualquer agência bancária até o vencimento.'],
-          numeroDocumento: '087762/2',
-          especieDocumento: 'DM',
-          localDoPagamento: 'Minha casa',
-          valor: 879,
-          datas: {
-            vencimento: '2024-06-03',
-            processamento: '2024-04-04',
-            documentos: '2024-04-04'
-          }
-        }
+          instrucoes: [
+            'Sacador/Avalista: SB CREDITO FIDC MULTISSETORIA',
+            'CPF/CNPJ: 23956882000169',
+            'COD CIVIL ART.308 -LEI 5.474/68 - ART8°',
+            'PROTESTO APÓS O 5° DIA DO VENCIMENTO',
+            '**VALORES EXPRESSOS EM REAIS**',
+            '///ATENÇÃO ////SEGUNDA VIA'
+          ],
+          beneficiario: {
+            nome: 'SB CREDITO FIDC MULTISSETORIA',
+            cnpj: '23956882000169',
+            dadosBancarios: {
+              carteira: '112',
+              agencia: '3833',
+              agenciaDigito: '0',
+              conta: '66609',
+              contaDigito: '6',
+              nossoNumero: '94497670',
+              nossoNumeroDigito: '6'
+            },
+            endereco: {
+              logradouro: 'Rua Alves Guimarães, 1212',
+              bairro: 'Pinheiros',
+              cidade: 'São Paulo',
+              estadoUF: 'SP',
+              cep: '05410002'
+            }
+          },
+          boleto: {
+            numeroDocumento: '000240682D',
+            especieDocumento: 'DM',
+            valor: 1182.86,
+            datas: {
+              vencimento: '2024-05-30',
+              processamento: '2024-02-22',
+              documentos: '2024-02-22'
+            }
+          }         
 };
 
 const novoBoleto = new Boletos(boleto);
 novoBoleto.gerarBoleto();
 
-
-// const pdf = `${randomUUID()}.pdf`;
-
-// const stream = fs.createWriteStream(`./tmp/boletos/${pdf}`);
-
-// novoBoleto.pdfStream(stream);
-
-
-novoBoleto.pdfFile().then(async ({stream}) => {
-    // ctx.res.set('Content-type', 'application/pdf');
-    await streamToPromise(stream);
-}).catch((error) => {
+novoBoleto.pdfFile().catch((error) => {
     return error;
 });
 
---
+++ examples/gerar-boleto-bradesco3.js
@@ -0,0 +1,94 @@
+const { randomUUID } = require('crypto');
+const {Bancos, Boletos, streamToPromise} = require('../lib/index');
+const fs = require('fs');
+
+const boleto = {
+        banco: new Bancos.Bradesco(),
+          pagador: {
+            nome: 'LKR MATERIAIS PARA CONSTRUCAO LTDA',
+            RegistroNacional: '02654560000195',
+            endereco: {
+              logradouro: 'AV MARGINAL 1192',
+              bairro: 'JARDIM ANITA CASELLI',
+              cidade: 'MAGDA',
+              estadoUF: 'SP',
+              cep: '15310000'
+            }
+          },
+          instrucoes: [
+            'JUROS POR DIA DE ATRASO R$ 2.98',
+            'APOS VENCIMENTO, MULTA DE R$ 0',
+            '',
+            'Caso não seja efetuado o pagamento em até 5 dias úteis após o vencimento,',
+            'o mesmo será protestado em cartório.'
+          ],
+          beneficiario: {
+            nome: 'FERRAGENS 3F DO BRASIL EIRELI',
+            cnpj: '02464189000107',
+            dadosBancarios: {
+              carteira: '09',
+              agencia: '1332',
+              agenciaDigito: '3',
+              conta: '0107371',
+              contaDigito: '',
+              nossoNumero: '40640000060',
+              nossoNumeroDigito: '2'
+            },
+            endereco: { logradouro: '', bairro: '', cidade: '', estadoUF: '', cep: '' }
+          },
+          boleto: {
+            numeroDocumento: '000241234',
+            especieDocumento: 'DM',
+            valor: 994.89,
+            datas: {
+              vencimento: '2024-05-06',
+              processamento: '2024-03-04',
+              documentos: '2024-03-04'
+            }
+          }
+};
+
+const novoBoleto = new Boletos(boleto);
+novoBoleto.gerarBoleto();
+
+novoBoleto.pdfFile(undefined, undefined, function (pdf, x, y, sac) {
+  pdf.font('negrito')
+  .fontSize(8)
+  .text('Sacador/Avalista:',sac.x + 11, sac.y - 26, {
+      lineBreak: false,
+      width: 294,
+      align: 'left'
+  });
+
+  pdf.font('negrito')
+  .fontSize(8)
+  .text('Sacador/Avalista:',sac.x + 11, sac.y - 366, {
+      lineBreak: false,
+      width: 294,
+      align: 'left'
+  });
+
+  pdf.font('normal')
+  .fontSize(8)
+  .text('FERRAGENS 3F DO BRASIL EIRELI / CNPJ: 02.464.189/0001-07',sac.x + 81, sac.y - 366, {
+      lineBreak: false,
+      width: 294,
+      align: 'left'
+  });
+
+
+  pdf.font('normal')
+  .fontSize(8)
+  .text('FERRAGENS 3F DO BRASIL EIRELI / CNPJ: 02.464.189/0001-07',sac.x + 91, sac.y - 26, {
+      lineBreak: false,
+      width: 294,
+      align: 'left'
+  });
+
+}, true).then(async (result) => {
+
+    console.log(result);
+}).catch((error) => {
+    return error;
+});
+
