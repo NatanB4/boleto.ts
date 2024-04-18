f353a7c5228c8f117d9b81f66b8816c3134d6691
natanb4 <natanbarbosa027@gmail.com>
2024-04-18 16:59:15 -0300

    feat: add try catch

--- examples/gerar-boleto-bradesco.js
+++ examples/gerar-boleto-bradesco.js
@@ -1,62 +1,63 @@
-const {Bancos, Boletos } = require('../lib/index');
+const { Bancos, Boletos } = require('../lib/index');
 
 const boleto = {
-        banco: new Bancos.Itau(),
-          pagador: {
-            nome: 'EFM COMERCIO DE MATERIAIS DE C',
-            RegistroNacional: '30.340.910/0001-84',
-            endereco: {
-              logradouro: 'R CONSELHEIRO FRANCO, 295',
-              bairro: 'CENTRO',
-              cidade: 'FEIRA DE SANTAN',
-              estadoUF: 'BA',
-              cep: '44002128'
-            }
-          },
-          instrucoes: [
-            'Sacador/Avalista: SB CREDITO FIDC MULTISSETORIA',
-            'CPF/CNPJ: 23956882000169',
-            'COD CIVIL ART.308 -LEI 5.474/68 - ART8°',
-            'PROTESTO APÓS O 5° DIA DO VENCIMENTO',
-            '**VALORES EXPRESSOS EM REAIS**',
-            '///ATENÇÃO ////SEGUNDA VIA'
-          ],
-          beneficiario: {
-            nome: 'SB CREDITO FIDC MULTISSETORIA',
-            cnpj: '23956882000169',
-            dadosBancarios: {
-              carteira: '112',
-              agencia: '3833',
-              agenciaDigito: '0',
-              conta: '66609',
-              contaDigito: '6',
-              nossoNumero: '94497670',
-              nossoNumeroDigito: '6'
-            },
-            endereco: {
-              logradouro: 'Rua Alves Guimarães, 1212',
-              bairro: 'Pinheiros',
-              cidade: 'São Paulo',
-              estadoUF: 'SP',
-              cep: '05410002'
-            }
-          },
-          boleto: {
-            numeroDocumento: '000240682D',
-            especieDocumento: 'DM',
-            valor: 1182.86,
-            datas: {
-              vencimento: '2024-05-30',
-              processamento: '2024-02-22',
-              documentos: '2024-02-22'
-            }
-          }         
+  banco: new Bancos.Itau(),
+  pagador: {
+    nome: 'EFM COMERCIO DE MATERIAIS DE C',
+    RegistroNacional: '30.340.910/0001-84',
+    endereco: {
+      logradouro: 'R CONSELHEIRO FRANCO, 295',
+      bairro: 'CENTRO',
+      cidade: 'FEIRA DE SANTAN',
+      estadoUF: 'BA',
+      cep: '44002128'
+    }
+  },
+  instrucoes: [
+    'Sacador/Avalista: SB CREDITO FIDC MULTISSETORIA',
+    'CPF/CNPJ: 23956882000169',
+    'COD CIVIL ART.308 -LEI 5.474/68 - ART8°',
+    'PROTESTO APÓS O 5° DIA DO VENCIMENTO',
+    '**VALORES EXPRESSOS EM REAIS**',
+    '///ATENÇÃO ////SEGUNDA VIA'
+  ],
+  beneficiario: {
+    nome: 'SB CREDITO FIDC MULTISSETORIA',
+    cnpj: '23956882000169',
+    dadosBancarios: {
+      carteira: '112',
+      agencia: '38332',
+      agenciaDigito: '0',
+      conta: '6660922',
+      contaDigito: '6',
+      nossoNumero: '94497670',
+      nossoNumeroDigito: '6'
+    },
+    endereco: {
+      logradouro: 'Rua Alves Guimarães, 1212',
+      bairro: 'Pinheiros',
+      cidade: 'São Paulo',
+      estadoUF: 'SP',
+      cep: '05410002'
+    }
+  },
+  boleto: {
+    numeroDocumento: '000240682D',
+    especieDocumento: 'DM',
+    valor: 1182.86,
+    datas: {
+      vencimento: '2024-05-30',
+      processamento: '2024-02-22',
+      documentos: '2024-02-22'
+    }
+  }
 };
 
 const novoBoleto = new Boletos(boleto);
 novoBoleto.gerarBoleto();
 
-novoBoleto.pdfFile().catch((error) => {
-    return error;
+novoBoleto.pdfFile().catch((err) => {
+
+  // return err;
 });
 
